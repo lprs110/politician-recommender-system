e6079361702728ae912ea0a4f9a5663b420f30cf
Lucas Pereira Reis <lprs.eng@uea.edu.br>
2019-03-06 19:10:35 -0400

    feat: add form, action and view for register areas and rate candidates

--- app/controllers/site.py
+++ app/controllers/site.py
@@ -1,9 +1,9 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import login_user, logout_user
+from flask import render_template, flash, redirect, url_for, abort
+from flask_login import login_user, logout_user, current_user
 from app import app, db, login_manager
 
-from app.models.tables import User, Area, Candidate
-from app.models.forms import LoginForm, RegisterForm
+from app.models.tables import User, Area, Candidate, User_Areas, User_Candidates
+from app.models.forms import LoginForm, RegisterForm, RegisterAreasForm, RateCandidatesForm
 
 
 @login_manager.user_loader
@@ -24,10 +24,9 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user and user.password == form.password.data:
             login_user(user)
-            flash("Login feito com sucesso")
             return redirect(url_for('index'))
         else:
-            flash("Login inválido")
+            flash("Login inválido", 'error')
 
     return render_template('login.html', form=form)
 
@@ -35,12 +34,13 @@ def login():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
+
     if form.validate_on_submit():
         new_user = User(form.username.data, form.password.data)
         db.session.add(new_user)
         db.session.commit()
-        flash("Cadastrado realizado com sucesso")
-        return redirect(url_for('index'))
+        flash("Cadastro feito com sucesso, complete seu perfil a seguir.", 'success')
+        return redirect(url_for('register_areas'))
 
     return render_template('register.html', form=form)
 
@@ -48,8 +48,67 @@ def register():
 @app.route("/logout")
 def logout():
     logout_user()
-    flash("Usuário deslogado")
     return redirect(url_for('index'))
+
+
+@app.route("/register_areas", methods=["GET", "POST"])
+def register_areas():
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+
+    form = RegisterAreasForm()
+
+    if form.validate_on_submit():
+
+        user = current_user
+
+        for field, value in form.data.items():
+
+            if field != "csrf_token":
+                area = Area.query.filter_by(area_name=field).first()
+
+                user_area_relationship = User_Areas(user=user, area=area, rate=value)
+
+                db.session.add(user_area_relationship)
+
+        db.session.commit()
+
+        flash("Cadastrado com sucesso", 'success')
+
+        return redirect(url_for('index'))
+
+    return render_template('register_areas.html', form=form)
+
+
+@app.route("/rate_candidates", methods=["GET", "POST"])
+def rate_candidates():
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+
+    form = RateCandidatesForm()
+
+    if form.validate_on_submit():
+
+        user = current_user
+
+        for field, value in form.data.items():
+
+            if field != "csrf_token":
+                candidate = Candidate.query.filter_by(candname=field).first()
+
+                user_cand_relationship = User_Candidates(user=user, candidate=candidate, rate=value)
+
+                db.session.add(user_cand_relationship)
+
+        db.session.commit()
+
+        flash("Avaliação de candidatos feita com sucesso.", 'success')
+
+        return redirect(url_for('index'))
+
+    return render_template('rate_candidates.html', form=form)
 
 '''
 @app.route("/create_area")
--- app/models/forms.py
+++ app/models/forms.py
@@ -1,13 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
 
 
 class LoginForm(FlaskForm):
-    username = StringField("username", validators=[DataRequired()])
-    password = PasswordField("password", validators=[DataRequired()])
+    username = StringField(label="Usuário", validators=[DataRequired()])
+    password = PasswordField(label="Senha", validators=[DataRequired()])
 
 
 class RegisterForm(FlaskForm):
     username = StringField(label="Usuário", validators=[DataRequired()])
     password = PasswordField(label="Senha", validators=[DataRequired()])
+
+
+class RegisterAreasForm(FlaskForm):
+    cultura = IntegerField(label="Cultura", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    economia = IntegerField(label="Economia", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    educacao = IntegerField(label="Educação", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    meio_ambiente = IntegerField(label="Meio Ambiente", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    saude = IntegerField(label="Saúde", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    seguranca = IntegerField(label="Segurança", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    tecnologia = IntegerField(label="Tecnologia", validators=[DataRequired(), NumberRange(min=1, max=5)])
+
+
+class RateCandidatesForm(FlaskForm):
+    alckmin = IntegerField(label="Geraldo Alckmin", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    amoedo = IntegerField(label="João Amoêdo", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    bolsonaro = IntegerField(label="Jair Bolsonaro", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    ciro = IntegerField(label="Ciro Gomes", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    daciolo = IntegerField(label="Cabo Daciolo", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    boulos = IntegerField(label="Guilherme Boulos", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    haddad = IntegerField(label="Fernando Haddad", validators=[DataRequired(), NumberRange(min=1, max=5)])
+    marina = IntegerField(label="Marina Silva", validators=[DataRequired(), NumberRange(min=1, max=5)])
