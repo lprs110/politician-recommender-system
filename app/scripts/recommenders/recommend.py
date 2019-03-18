efc9f30bd386d7ec2ccf8e09af55efcf2240132b
Lucas Pereira Reis <lprs.eng@uea.edu.br>
2019-03-18 19:18:16 -0400

    feat: add new condition to show candidates
    Only the candidates with a rate greater than 3.0 will be used in content based filtering. If has no candidates, the condition will be greater than 1.0

--- app/scripts/recommenders/recommend.py
+++ app/scripts/recommenders/recommend.py
@@ -1,6 +1,7 @@
 from app.scripts.recommenders.content_based_filtering import cb_recommend
 from app.scripts.recommenders.collaborative_filtering import col_recommend
 import sqlite3
+from math import *
 
 con = sqlite3.connect('storage.db', check_same_thread=False)
 cur = con.cursor()
@@ -9,8 +10,20 @@ cur = con.cursor()
 def Hybrid_Recommendation(id):
     user = cur.execute("select * from users where id=?", (id,)).fetchall()
 
-    col_recommendations = col_recommend(user)
+    col_rec = col_recommend(user)
 
-    hybrid_recommendation = cb_recommend(col_recommendations, user)
+    rec = []
+
+    max_rate = max(col_rec, key=lambda rate: rate[1])
+    if ceil(max_rate[1]) >= 3.0:
+        for i, t in enumerate(col_rec):
+            if round(t[1]) >= 2.5:
+                rec.append(t)
+    else:
+        for i, t in enumerate(col_rec):
+            if round(t[1]) > 1.0:
+                rec.append(t)
+
+    hybrid_recommendation = cb_recommend(rec, user)
 
     return hybrid_recommendation
